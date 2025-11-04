ffbee636efe7022ee9f4bb8dc1be294a326e0d16
shimarda <ryo1031shima@gmail.com>
2025-11-04 22:16:41 +0900

    poll終了

---
+++ 06/64bitkernel/poll.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <poll.h>
+
+
+#define DEVFILE "/dev/devone0"
+
+int main(void)
+{
+        int fd;
+        struct pollfd fds;
+        int retval;
+        unsigned char buf[64];
+        ssize_t sz;
+        int i;
+        
+        fd = open(DEVFILE, O_RDWR);
+        if (fd == -1) {
+                perror("open");
+                exit(1);
+        }
+        
+        do {
+                fds.fd = fd;
+                fds.events = POLLIN;
+                
+                printf("poll() ...\n");
+                retval = poll(&fds, 1, 5 * 1000);
+                if (retval == -1) {
+                        perror("poll");
+                        break;
+                }
+
+                if (retval) {
+                        break;
+                }
+        } while (retval == 0);
+
+        if (fds.revents & POLLIN) {
+                printf("read() ...\n");
+                sz = read(fd, buf, sizeof(buf));
+                printf("read() %ld\n", sz);
+                for (i = 0; i < sz; i++) {
+                        printf("%02x ", buf[i]);
+                }
+        }
+        close(fd);
+
+        return 0;
+}
